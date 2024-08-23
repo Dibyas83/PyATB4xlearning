58ef6d61bc0b896f5200ae08e9b88492ab8fd995
a2z <dibyas83@gmail.com>
2024-08-23 16:52:34 +0530

    ist lab

---
+++ src/ex.08082024/lab001.py
@@ -0,0 +1,6 @@
+print("hello pramod,welcome to the learning python.")
+print(2+2)
+  # sep='' - how you want to separate the arguments
+  # end='\n'  - in the end what you want to do
+print("Hello","World",123,3.14,True,sep="|",end=" ")
+print("Pramod")
