e8a828b6f911740957b796ba605afb8a3a4802fd
a2z <dibyas83@gmail.com>
2024-08-23 17:19:07 +0530

    ist lab

--- src/ex.08082024/lab002.py
+++ src/ex.08082024/lab002.py
@@ -1,2 +1,10 @@
 print("good","pramod",sep="?",end="_")
 print("Amit")
+print(max(34,44,66,-99))
+  # strings can't be added in between int
+  # import keyword
+  # print(keyword,kwlist)
+  # variable name ?
+  # it is used to store the value in containers(var)
+age = 65
+print(age)
